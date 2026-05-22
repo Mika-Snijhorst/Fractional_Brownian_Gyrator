4ea0132e970e305eacf7434f9afa91dcd4097dab
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2026-05-22 17:28:12 +0200

    added amplitudes of the noises and run some simultations

--- FBG_trajectories.py
+++ FBG_trajectories.py
@@ -2,24 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mpmath
 from fbm import FBM
+from scipy.special import gamma
+from tqdm import tqdm
 
 #sets the precision of mpmath to 15 decimals  
 mpmath.mp.dps = 15
 
 #params
-k = 1
+k = 0
 eta_alpha = 1
 eta_nu = 1
 k_B = 1
+Tf = 5
+T0 = 1e-5
+dt = 0.0005
 
 #variables
-alpha = 0.8
-nu = 0.7
+alpha = 0.9
+nu = 0.9
+
+Hurst_x = 1 - alpha/2
+Hurst_y = 1 - nu/2
+
 T_x = 1
-T_y = 3
+T_y = 100
+
+A_x = np.sqrt(2*k_B*T_x*eta_alpha/gamma(2*Hurst_x+1))
+A_y = np.sqrt(2*k_B*T_y*eta_alpha/gamma(2*Hurst_y+1))
 
 #time array, we start from a small value because as t approaches 0, s approaches infinity.
-t_vals = np.linspace(1e-5, 0.5, 150)
+t_vals = np.arange(T0, Tf, dt)
 
 def G_hat(s):
     '''
@@ -43,7 +55,7 @@ def invert(func_hat, t_values):
     d_type of the output functions
     '''
     array = np.zeros_like(t_values)     
-    for i, t in enumerate(t_values):
+    for i, t in tqdm(enumerate(t_values)):
         if t<= 0:
             raise ValueError("Talbot requires t > 0")
         else:
@@ -52,25 +64,29 @@ def invert(func_hat, t_values):
 
 #finds the dt
 T = len(t_vals)
-dt = t_vals[1] - t_vals[0]
+
 
 #the fbm package uses the Hurst exponent, so we need to convert alpha and nu
-Hurst_x = 1 - alpha/2
-Hurst_y = 1 - nu/2
 
 #generates the FBM noise, t_vals[-1] gives the total time corresponding with T-1 steps
 fbm_x = FBM(n=T-1, hurst = Hurst_x, length = t_vals[-1], method = "daviesharte")
 fbm_y = FBM(n=T-1, hurst = Hurst_y, length = t_vals[-1], method = "daviesharte")
-f_x = fbm_x.fbm()
-f_y = fbm_y.fbm()
+fbm_x_traj = fbm_x.fbm()
+fbm_y_traj = fbm_y.fbm()
 
 #the noise needs to be normalized for the right time steps
-xi_x = np.diff(f_x) / dt
-xi_y = np.diff(f_y) / dt
+xi_x = np.diff(fbm_x_traj) / dt
+xi_y = np.diff(fbm_y_traj) / dt
+
+f_x = A_x*xi_x
+f_y = A_y*xi_y
 
 #the H functions from before
+print("Inverting H_dx")
 H_dx = invert(H_dx_hat, t_vals)
+print("Inverting H_dy")
 H_dy = invert(H_dy_hat, t_vals)
+print("Inverting H_c")
 H_c  = invert(H_c_hat, t_vals)
 
 #this simplifies the convolution just like 1.10
@@ -78,11 +94,11 @@ H_x = H_dx + H_c
 H_y = H_dy + H_c
 
 #this uses the convolution module included within numpy, we have to chip off the last value to match the noise array length
-pos_x = dt * (np.convolve(H_x, xi_x, mode ="full")[:len(t_vals) - 1]
-          + np.convolve(H_c, xi_y, mode= "full")[:len(t_vals) - 1])
+pos_x = dt * (np.convolve(H_x, f_x, mode ="full")[:len(t_vals) - 1]
+          + np.convolve(H_c, f_y, mode= "full")[:len(t_vals) - 1])
 
-pos_y = dt * (np.convolve(H_y, xi_y, mode='full')[:len(t_vals)-1]
-          + np.convolve(H_c, xi_x, mode='full')[:len(t_vals)-1])
+pos_y = dt * (np.convolve(H_y, f_y, mode='full')[:len(t_vals)-1]
+          + np.convolve(H_c, f_x, mode='full')[:len(t_vals)-1])
 
 #a bit crude but it makes it more intuitive to see the trajectory evolve in time
 colors = ["red", "orange", "yellow", "green", "blue", "purple", "magenta"]
@@ -90,12 +106,14 @@ ph = len(pos_x) // len(colors)
 for i in range(len(colors)):
     plt.scatter(pos_x[i*ph], pos_y[i*ph], color=colors[i], s = 6)
     plt.plot(pos_x[i*ph:(i+1)*ph+1], pos_y[i*ph:(i+1)*ph+1], color=colors[i], lw = "0.5")
+    
 
 #plotting settings
 plt.xlabel(r"$x(t)$")
 plt.ylabel(r"$y(t)$")
 plt.axvline(0, color = "black", lw = "0.5", alpha = 0.7)
 plt.axhline(0, color = "black", lw = "0.5", alpha = 0.7)
+plt.savefig(f"trajectory_alpha{alpha}nu{nu}-Tx{T_x}Ty{T_y}-k{k}_Tf{Tf}_dt{dt}.png")
 plt.show()
 
 
